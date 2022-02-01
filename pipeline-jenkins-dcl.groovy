1316fa837513c338d0308eacc5039545e92b63de
Swastik1993bond <swastikmukherjee276@gmail.com>
2022-02-01 11:30:13 +0530

    Updated sonarqube stage

--- pipeline-jenkins-dcl.groovy
+++ pipeline-jenkins-dcl.groovy
@@ -2,16 +2,23 @@ pipeline{
     agent any
     tools{
         maven 'Maven 3.3.9'
-        jdk 'jdk 8' }
+        jdk 'jdk 8'
+        sonarqube 'My SonarQube Server' }
         stages{
             stage('Checkout stage'){
                 steps{
                     git 'https://github.com/Swastik1993bond/docker.git'
                 }
-            stage('Build stage'){
+            stage('Build & Sonarqube Analysis stage'){
                 steps{
+                    withSonarQubeEnv('My SonarQube Server')
                     sh 'mvn -Dmaven.test.failure.ignore=true install'
                 }
+            stage("Quality Gate") {
+                steps {
+              timeout(time: 1, unit: 'HOURS') {
+                waitForQualityGate abortPipeline: true
+              }
             stage('Test Stage'){
                 steps{
                    junit 'myproject/target/test-reports/*.xml' 
